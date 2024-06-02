84db41082a17dc4791045dd3cb9935541fe3a329
Muhammed Bacalan <muhammedbacalan@gmail.com>
2024-06-02 23:07:51 +0200

    (vim): Update to trouble.nvim v3

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -2,13 +2,14 @@ require('telescope').load_extension('fzf')
 
 local telescope = require("telescope")
 local builtin = require('telescope.builtin')
-local trouble = require("trouble.providers.telescope")
+local actions = require("telescope.actions")
+local open_with_trouble = require("trouble.sources.telescope").open
 
 telescope.setup {
   defaults = {
     mappings = {
-      i = { ["<c-t>"] = trouble.open_with_trouble },
-      n = { ["<c-t>"] = trouble.open_with_trouble },
+      i = { ["<c-t>"] = open_with_trouble },
+      n = { ["<c-t>"] = open_with_trouble },
     },
   },
 }
