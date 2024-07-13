418c6cffc7eafac9a6fdc94d4c6b2935c9462fc3
Muhammed Bacalan <muhammedbacalan@gmail.com>
2024-07-13 14:47:21 +0200

    (vim): Restructure plugin setup per Lazy recommendations

---
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  'folke/twilight.nvim',
+  'nvim-tree/nvim-web-devicons',
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'tokyonight',
+          style = 'storm',
+          icons_enabled = false
+        }
+      }
+    end,
+  },
+}
