22e96f1051bd3fc4abbe519d6b02a5bfb9ace3e5
Muhammed Bacalan <muhammedbacalan@gmail.com>
2024-08-17 18:32:36 +0200

    (vim): Remove mini.starter
    Never used, always open into a project

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
     'echasnovski/mini.nvim',
     version = '*',
     config = function()
-      require('mini.starter').setup()
       require('mini.files').setup()
 
       -- Toggle explorer
