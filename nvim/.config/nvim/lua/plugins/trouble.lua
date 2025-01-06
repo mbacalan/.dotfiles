8f22735d4cc17dc0364e397e9594bd5aff35d684
Muhammed Bacalan <muhammedbacalan@gmail.com>
2025-01-06 22:14:43 +0100

    (vim): Adjust Trouble keys

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -2,38 +2,41 @@ return {
   {
     'folke/trouble.nvim',
     opts = {
-      focus = true
+      focus = true,
+      win = {
+        position = "bottom"
+      }
     },
     cmd = "Trouble",
     keys = {
       {
         "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
+        "<cmd>Trouble diagnostics toggle focus=true win.position=bottom<cr>",
         desc = "Diagnostics (Trouble)",
       },
       {
         "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        "<cmd>Trouble diagnostics toggle focus=true filter.buf=0 win.position=bottom<cr>",
         desc = "Buffer Diagnostics (Trouble)",
       },
       {
         "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
+        "<cmd>Trouble symbols toggle focus=true win.position=bottom<cr>",
         desc = "Symbols (Trouble)",
       },
       {
         "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        "<cmd>Trouble lsp toggle focus=true win.position=bottom<cr>",
         desc = "LSP Definitions / references / ... (Trouble)",
       },
       {
         "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
+        "<cmd>Trouble loclist toggle focus=true win.position=bottom<cr>",
         desc = "Location List (Trouble)",
       },
       {
         "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
+        "<cmd>Trouble qflist toggle focus=true win.position=bottom<cr>",
         desc = "Quickfix List (Trouble)",
       },
     },
