8957ae9488c10bcf49f8b9e8f4410e8c8e1bdda0
Muhammed Bacalan <muhammedbacalan@gmail.com>
2025-03-12 16:52:42 +0100

    (vim): Update Snacks config
    Keybind changes and statuscolumn enabled

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -6,30 +6,29 @@ return {
     opts = {
       bigfile = { enabled = true },
       indent = { enabled = true },
-      input = { enabled = true },
       picker = { enabled = true },
       scope = { enabled = true },
       words = { enabled = true },
       rename = { enabled = true },
-      -- explorer = { enabled = true },
-      -- notifier = { enabled = true },
-      -- quickfile = { enabled = true },
-      -- scroll = { enabled = true },
-      -- statuscolumn = { enabled = true },
+      statuscolumn = { enabled = true }
     },
     keys = {
       -- snacks.picker
-      { "<leader><space>", function() Snacks.picker.smart() end,            desc = "Smart Find Files" },
-      { "<leader>fs",      function() Snacks.picker.grep() end,             desc = "Grep" },
-      { "<leader>ff",      function() Snacks.picker.files() end,            desc = "Find Files" },
-      { "<leader>fg",      function() Snacks.picker.git_files() end,        desc = "Find Git Files" },
-      { "<leader>fe",      function() Snacks.explorer() end,                desc = "File Explorer" },
-      -- { "<leader>,",       function() Snacks.picker.buffers() end,         desc = "Buffers" },
-      -- { "<leader>:",       function() Snacks.picker.command_history() end, desc = "Command History" },
-      -- { "<leader>n",       function() Snacks.picker.notifications() end,   desc = "Notification History" },
+      { "<leader><space>", function() Snacks.picker.smart() end,                desc = "Smart Find Files" },
+      { "<leader>fs",      function() Snacks.picker.grep() end,                 desc = "Grep" },
+      { "<leader>ff",      function() Snacks.picker.files() end,                desc = "Find Files" },
+      { "<leader>fg",      function() Snacks.picker.git_files() end,            desc = "Find Git Files" },
+      { "<leader>fe",      function() Snacks.explorer() end,                    desc = "File Explorer" },
+      { "<leader>ss",      function() Snacks.picker.lsp_symbols() end,          desc = "LSP Symbols" },
       -- snacks.words
-      { "]]",              function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",  mode = { "n", "t" } },
-      { "[[",              function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",  mode = { "n", "t" } },
+      { "]]",              function() Snacks.words.jump(vim.v.count1) end,      desc = "Next Reference",        mode = { "n", "t" } },
+      { "[[",              function() Snacks.words.jump(-vim.v.count1) end,     desc = "Prev Reference",        mode = { "n", "t" } },
+      -- lsp
+      { "gd",              function() Snacks.picker.lsp_definitions() end,      desc = "Goto Definition" },
+      { "gD",              function() Snacks.picker.lsp_declarations() end,     desc = "Goto Declaration" },
+      { "gr",              function() Snacks.picker.lsp_references() end,       nowait = true,                  desc = "References" },
+      { "gi",              function() Snacks.picker.lsp_implementations() end,  desc = "Goto Implementation" },
+      { "gy",              function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
     }
   }
 }
