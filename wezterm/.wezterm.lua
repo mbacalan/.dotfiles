2e96c9a78dcb3c6d89a535f0b9168e7b416605a5
Muhammed Bacalan <muhammedbacalan@gmail.com>
2026-03-18 22:46:48 +0100

    (term): Use higher font size for darwin

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,53 +1,54 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local function get_domain_name(tab)
-  local domain = tab.active_pane.domain_name
-  if domain == "local" then
-    return ""
-  end
-  return "[" .. domain .. "] "
+	local domain = tab.active_pane.domain_name
+	if domain == "local" then
+		return ""
+	end
+	return "[" .. domain .. "] "
 end
 
 -- Events
 wezterm.on("format-tab-title", function(tab)
-  local title = get_domain_name(tab)
-  title = title .. tab.active_pane.title
+	local title = get_domain_name(tab)
+	title = title .. tab.active_pane.title
 
-  return {
-    { Text = " " .. title .. " " },
-  }
+	return {
+		{ Text = " " .. title .. " " },
+	}
 end)
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = "Tokyo Night"
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.font = wezterm.font_with_fallback {
-  'Cascadia Code NF', 'Segoe UI Emoji'
-}
-config.font_size = 13
+config.font = wezterm.font_with_fallback({
+	"Cascadia Code NF",
+	"Segoe UI Emoji",
+})
+config.font_size = wezterm.target_triple:find("darwin") and 16 or 13
 
 config.ssh_domains = {
-  {
-    name = 'SSH Optiplex',
-    remote_address = '192.168.178.250',
-    username = 'mbacalan',
-    multiplexing = 'None'
-  },
-  {
-    name = 'RPi',
-    remote_address = '192.168.178.35',
-    username = 'mbacalan',
-    multiplexing = 'None'
-  }
+	{
+		name = "SSH Optiplex",
+		remote_address = "192.168.178.250",
+		username = "mbacalan",
+		multiplexing = "None",
+	},
+	{
+		name = "RPi",
+		remote_address = "192.168.178.35",
+		username = "mbacalan",
+		multiplexing = "None",
+	},
 }
 
 config.max_fps = 60
 config.animation_fps = 60
-config.front_end = 'WebGpu'
+config.front_end = "WebGpu"
 
 -- and finally, return the configuration to wezterm
 return config
