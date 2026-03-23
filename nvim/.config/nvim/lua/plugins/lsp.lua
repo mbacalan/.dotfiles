1cfa8d889d730fbf85d9ce501746457017dc2456
Muhammed Bacalan <muhammedbacalan@gmail.com>
2026-03-23 19:46:06 +0100

    (vim): Fix typos in lsp config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -16,8 +16,8 @@ return {
 			keymap = {
 				preset = "default",
 				["<CR>"] = { "accept", "fallback" },
-				["<Tab>"] = { "select_next", "snipper_forward", "fallback" },
-				["<S-Tab>"] = { "select_prev", "snipper_backward", "falllback" },
+				["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+				["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
 			},
 			appearance = {
 				nerd_font_variant = "mono",
